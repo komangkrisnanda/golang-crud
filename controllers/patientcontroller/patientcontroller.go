aa769c16e13dff609f8d41f013da36de4d1f114a
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-16 14:17:02 +0800

    add: store form

--- controllers/patientcontroller/patientcontroller.go
+++ controllers/patientcontroller/patientcontroller.go
@@ -15,7 +15,12 @@ func Index(response http.ResponseWriter, request *http.Request){
 
 
 func Add(response http.ResponseWriter, request *http.Request){
-	
+	temp, err := template.ParseFiles("views/patient/add.html")
+	if err != nil {
+		panic(err)
+	}
+	temp.Execute(response, nil)
+
 }
 
 
